1ffbdbafc6d56b1b71c7d163205b2bb6d43bf960
Blake Stickland <blakelists@yahoo.com.au>
2022-04-27 09:15:45 +1000

    inital commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component  {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+    console.log("1st");
+  }
+
+  componentDidMount() {
+    console.log("3rd");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then(users => this.setState(() => {
+        return {monsters: users}
+      },
+      () => {
+        console.log(this.state);
+      }));
+  }
+
+  render() {
+    console.log("2nd");
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
