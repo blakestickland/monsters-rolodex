ac5e1d9da530b4c2f74cfc692e529df76a32769d
Blake Stickland <blakelists@yahoo.com.au>
2022-04-27 11:35:23 +1000

    can use search field to search through the names of the monsters with results displayed on change.

--- src/App.js
+++ src/App.js
@@ -6,27 +6,56 @@ class App extends Component  {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
-    console.log("1st");
+    console.log("constructor");
   }
 
   componentDidMount() {
-    console.log("3rd");
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then(users => this.setState(() => {
-        return {monsters: users}
-      },
-      () => {
-        console.log(this.state);
-      }));
+      .then(users => 
+        this.setState(
+          () => {
+            return {monsters: users}
+          },
+        () => {
+          console.log(this.state);
+          }
+      )  
+    );
   }
 
   render() {
-    console.log("2nd");
+    console.log("render");
+
+    function filterItems(arr, query) {
+      return arr.filter((element) => {
+        const queryString = query.toLowerCase();
+        return element.name.toLowerCase().indexOf(queryString) !== -1;
+      })
+    }
+
+    const filteredMonsters = filterItems(
+      this.state.monsters,
+      this.state.searchField
+    );
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            const searchField = event.target.value.toLowerCase();
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
