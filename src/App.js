08fee4f35932a534534c18886b09a49e0d4422c1
Blake Stickland <blakelists@yahoo.com.au>
2022-04-27 23:19:04 +1000

    start of refactor in App.js to use hooks beginning with useState

--- src/App.js
+++ src/App.js
@@ -1,72 +1,70 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
-class App extends Component  {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-    console.log("constructor");
-  }
+const App = () => {
+  const [searchField, setSearchField] = useState(""); // [value, setValue]
+  const [monsters, setMonsters] = useState([]);
 
-  componentDidMount() {
-    console.log("componentDidMount");
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then(users => 
-        this.setState(
-          () => {
-            return {monsters: users}
-          },
-        () => {
-          console.log(this.state);
-          }
-      )  
-    );
-  }
+  console.log("render");
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLowerCase();
-    this.setState(() => {
-      return { searchField };
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then(users =>
+      setMonsters(() => {
+        return {users};
+      })
+  );
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  const filterItems = (arr, query) => {
+    return arr.filter((element) => {
+      const queryString = query.toLowerCase();
+      return element.name.toLowerCase().indexOf(queryString) !== -1;
     });
   };
 
-  render() {
-    console.log("render");
+  const filteredMonsters = filterItems(monsters, searchField);
 
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        className="monsters-search-box"
+        placeholder="search-monsters"
+        onChangeHandler={onSearchChange}
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+}  
 
-    function filterItems(arr, query) {
-      return arr.filter((element) => {
-        const queryString = query.toLowerCase();
-        return element.name.toLowerCase().indexOf(queryString) !== -1;
-      })
-    }
+// constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchField: "",
+//     };
+//     console.log("constructor");
+//   }
 
-    const filteredMonsters = filterItems(
-      monsters,
-      searchField
-    );
+//   componentDidMount() {
+//     console.log("componentDidMount");
+//   }
 
+  
 
-    return (
-      <div className="App">
-      <h1 className='app-title'>Monsters Rolodex</h1>
-        <SearchBox 
-          className="monsters-search-box"
-          placeholder="search-monsters"
-          onChangeHandler={onSearchChange} 
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+//   render() {
+//     console.log("render");
+
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+// }
 
 export default App;
